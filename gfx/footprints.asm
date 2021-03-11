f807be9cde8a56360f00541145633cb8b6192256
PiaCarrot <datingsimsrs@gmail.com>
2021-03-11 14:13:26 -0500

    251-386 Footprints

--- gfx/footprints.asm
+++ gfx/footprints.asm
@@ -249,11 +249,6 @@ INCBIN "gfx/footprints/tyranitar.1bpp"
 INCBIN "gfx/footprints/lugia.1bpp"
 INCBIN "gfx/footprints/ho_oh.1bpp"
 INCBIN "gfx/footprints/celebi.1bpp"
-INCBIN "gfx/footprints/252.1bpp"
-INCBIN "gfx/footprints/253.1bpp"
-INCBIN "gfx/footprints/254.1bpp"
-INCBIN "gfx/footprints/255.1bpp"
-INCBIN "gfx/footprints/256.1bpp"
 ;===============================
 ;	Hoenn Footprints Start Here
 ;===============================
