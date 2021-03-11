682195109035717b5bb32fb9004a2a6f80ae7c5e
Monstarules <36967886+Monstarules@users.noreply.github.com>
2021-03-11 15:55:43 -0500

    Adding a 4th stats screen page
    Pretty standard from the Pret tutorials. The text on the 4th page is just a todo. I want to be able to prove that I have a working PV generated eventually.

--- engine/pokemon/stats_screen.asm
+++ engine/pokemon/stats_screen.asm
@@ -1,8 +1,9 @@
-	const_def 1
-	const PINK_PAGE  ; 1
-	const GREEN_PAGE ; 2
-	const BLUE_PAGE  ; 3
-NUM_STAT_PAGES EQU const_value + -1
+	const_def
+	const PINK_PAGE   ; 0
+	const GREEN_PAGE  ; 1
+	const BLUE_PAGE   ; 2
+	const ORANGE_PAGE ; 3
+NUM_STAT_PAGES EQU const_value
 
 BattleStatsScreenInit:
 	ld a, [wLinkMode]
@@ -60,12 +61,7 @@ StatsScreenInit_gotaddress:
 StatsScreenMain:
 	xor a
 	ld [wJumptableIndex], a
-	; stupid interns
-	ld [wcf64], a
-	ld a, [wcf64]
-	and %11111100
-	or 1
-	ld [wcf64], a
+	ld [wcf64], a ; PINK_PAGE
 .loop
 	ld a, [wJumptableIndex]
 	and $ff ^ (1 << 7)
@@ -80,12 +76,7 @@ StatsScreenMain:
 StatsScreenMobile:
 	xor a
 	ld [wJumptableIndex], a
-	; stupid interns
-	ld [wcf64], a
-	ld a, [wcf64]
-	and %11111100
-	or 1
-	ld [wcf64], a
+	ld [wcf64], a ; PINK_PAGE again
 .loop
 	farcall Mobile_SetOverworldDelay
 	ld a, [wJumptableIndex]
@@ -334,20 +325,22 @@ StatsScreen_JoypadAction:
 
 .a_button
 	ld a, c
-	cp BLUE_PAGE ; last page
+	cp ORANGE_PAGE ; last page
 	jr z, .b_button
 .d_right
 	inc c
-	ld a, BLUE_PAGE ; last page
+	ld a, ORANGE_PAGE ; last page
 	cp c
 	jr nc, .set_page
 	ld c, PINK_PAGE ; first page
 	jr .set_page
 
 .d_left
+	ld a, c
 	dec c
+	and a
 	jr nz, .set_page
-	ld c, BLUE_PAGE ; last page
+	ld c, ORANGE_PAGE ; last page
 	jr .set_page
 
 .done
@@ -473,7 +466,7 @@ StatsScreen_PlaceHorizontalDivider:
 	ret
 
 StatsScreen_PlacePageSwitchArrows:
-	hlcoord 12, 6
+	hlcoord 10, 6
 	ld [hl], "◀"
 	hlcoord 19, 6
 	ld [hl], "▶"
@@ -529,7 +522,6 @@ StatsScreen_LoadGFX:
 .PageTilemap:
 	ld a, [wcf64]
 	maskbits NUM_STAT_PAGES
-	dec a
 	ld hl, .Jumptable
 	rst JumpTable
 	ret
@@ -539,6 +531,7 @@ StatsScreen_LoadGFX:
 	dw .PinkPage
 	dw .GreenPage
 	dw .BluePage
+	dw .OrangePage
 
 .PinkPage:
 	hlcoord 0, 9
@@ -780,6 +773,15 @@ StatsScreen_LoadGFX:
 	dw wOTPartyMonOT
 	dw sBoxMonOT
 	dw wBufferMonOT
+
+.OrangePage:
+	ld de, PVString
+	hlcoord 1, 9
+	call PlaceString
+	ret
+
+PVString:
+	db "PV: TODO@"
 
 IDNoString:
 	db "<ID>№.@"
@@ -1076,8 +1078,11 @@ StatsScreen_AnimateEgg:
 	ret
 
 StatsScreen_LoadPageIndicators:
+	hlcoord 11, 5
+	ld a, $36 ; " " " "
+	call .load_square
 	hlcoord 13, 5
-	ld a, $36 ; first of 4 small square tiles
+	ld a, $36 ; " " " "
 	call .load_square
 	hlcoord 15, 5
 	ld a, $36 ; " " " "
@@ -1086,13 +1091,19 @@ StatsScreen_LoadPageIndicators:
 	ld a, $36 ; " " " "
 	call .load_square
 	ld a, c
+	cp PINK_PAGE
+	hlcoord 11, 5
+	jr z, .load_highlighted_square
 	cp GREEN_PAGE
+	hlcoord 13, 5
+	jr z, .load_highlighted_square
+	cp BLUE_PAGE
+	hlcoord 15, 5
+	jr z, .load_highlighted_square
+	; must be ORANGE_PAGE
+	hlcoord 17, 5
+.load_highlighted_square
 	ld a, $3a ; first of 4 large square tiles
-	hlcoord 13, 5 ; PINK_PAGE (< GREEN_PAGE)
-	jr c, .load_square
-	hlcoord 15, 5 ; GREEN_PAGE (= GREEN_PAGE)
-	jr z, .load_square
-	hlcoord 17, 5 ; BLUE_PAGE (> GREEN_PAGE)
 .load_square
 	push bc
 	ld [hli], a
